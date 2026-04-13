0a7978268242e3f5a3edf549dcb0bf4910260b86
KarlKhoa <khoavokm@gmail.com>
2026-04-13 14:51:02 +0800

    Weapons repository fixed

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -9,27 +9,9 @@ public abstract class Weapon : MonoBehaviour
     public float cooldownTime;
     public float lastFired = -9999f;
     public int baseDamage;
-    public bool isGun;
     //put bullet here
     public GameObject bullet;
-
-    private GameObject thisObject;
     
-    void Start() 
-    {
-            
-    }
 
-    public virtual void Fire()
-    {
-        if (isGun == true)
-        {
-            Instantiate(bullet);
-        }
-        else if( this == enabled)
-        {
-
-        }
-
-    }
+    public virtual void Fire(){}
 }
